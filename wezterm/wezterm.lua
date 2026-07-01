9ecc861a14f4ca2e9672b16e47617005cba12aa7
jeskay <hrvp@mail.ru>
2026-07-01 13:47:18 +0300

    wezterm keymap
    added keymaps for wezterm leader aky and pane splitting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,29 +3,52 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 -- Force modern key protocol
 config.enable_kitty_keyboard = true
+-- Leader Key
+config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
 
 config.keys = {
-  {
-    key = "Backspace",
-    action = wezterm.action.SendString("\u{7f}"),
-  },
-  -- Fix for numpads
-  {
-    key = 'Numpad3',
-    mods = 'CTRL',
-    action = wezterm.action.ActivateTabRelative(1)
-  },
-  -- Brute-force catch-all for Numpad PageUp (Previous Tab)
-  { key = 'PageUp',   mods = 'CTRL',       action = wezterm.action.ActivateTabRelative(-1) },
-  { key = 'PageUp',   mods = 'CTRL|SHIFT', action = wezterm.action.ActivateTabRelative(-1) },
-  { key = 'Numpad9',  mods = 'CTRL',       action = wezterm.action.ActivateTabRelative(-1) },
-  { key = 'Numpad9',  mods = 'CTRL|SHIFT', action = wezterm.action.ActivateTabRelative(-1) },
+  -- Pane splitting
+	{
+		key = "v",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "s",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "x",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+	-- Fix for backspace in zshell
+	{
+		key = "Backspace",
+		action = wezterm.action.SendString("\u{7f}"),
+	},
+	-- Fix for numpads
+	{
+		key = "Numpad3",
+		mods = "CTRL",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	-- Brute-force catch-all for Numpad PageUp (Previous Tab)
+	{ key = "PageUp", mods = "CTRL", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "PageUp", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "Numpad9", mods = "CTRL", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "Numpad9", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
 
-  -- Brute-force catch-all for Numpad PageDown (Next Tab)
-  { key = 'PageDown', mods = 'CTRL',       action = wezterm.action.ActivateTabRelative(1) },
-  { key = 'PageDown', mods = 'CTRL|SHIFT', action = wezterm.action.ActivateTabRelative(1) },
-  { key = 'Numpad3',  mods = 'CTRL',       action = wezterm.action.ActivateTabRelative(1) },
-  { key = 'Numpad3',  mods = 'CTRL|SHIFT', action = wezterm.action.ActivateTabRelative(1) }
+	-- Brute-force catch-all for Numpad PageDown (Next Tab)
+	{ key = "PageDown", mods = "CTRL", action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "PageDown", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "Numpad3", mods = "CTRL", action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "Numpad3", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
 }
 
 config.font_size = 14
